b0496d11b4dd9ab3a8624baf066f3f70b4facd3e
Clevsson <clevsson.ti@gmail.com>
2024-06-15 17:37:48 -0300

    Adicionado Soma e Subtração

---
+++ src/FormCalculadora.dfm
@@ -0,0 +1,66 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 442
+  ClientWidth = 628
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  TextHeight = 15
+  object lblIgual: TLabel
+    Left = 414
+    Top = 11
+    Width = 8
+    Height = 15
+    Caption = '='
+  end
+  object lblResultado: TLabel
+    Left = 428
+    Top = 9
+    Width = 7
+    Height = 17
+    Caption = '0'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -13
+    Font.Name = 'Segoe UI'
+    Font.Style = [fsBold]
+    ParentFont = False
+  end
+  object edtValor1: TEdit
+    Left = 152
+    Top = 8
+    Width = 121
+    Height = 23
+    TabOrder = 0
+  end
+  object edtValor2: TEdit
+    Left = 287
+    Top = 8
+    Width = 121
+    Height = 23
+    TabOrder = 1
+  end
+  object btnSomar: TButton
+    Left = 152
+    Top = 37
+    Width = 75
+    Height = 25
+    Caption = '+'
+    TabOrder = 2
+    OnClick = btnSomarClick
+  end
+  object btnSubtrair: TButton
+    Left = 152
+    Top = 72
+    Width = 75
+    Height = 25
+    Caption = '-'
+    TabOrder = 3
+    OnClick = btnSubtrairClick
+  end
+end
---
+++ src/FormCalculadora.pas
@@ -0,0 +1,72 @@
+unit FormCalculadora;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls;
+
+type
+  TForm1 = class(TForm)
+    edtValor1: TEdit;
+    edtValor2: TEdit;
+    lblIgual: TLabel;
+    lblResultado: TLabel;
+    btnSomar: TButton;
+    btnSubtrair: TButton;
+    procedure btnSomarClick(Sender: TObject);
+    procedure btnSubtrairClick(Sender: TObject);
+
+
+  private
+    { Private declarations }
+
+    function somar(valor1, valor2 : Float32): Float32;
+    function subtrair(valor1, valor2 : Float32) : Float32;
+
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+  procedure TForm1.btnSomarClick(Sender: TObject);
+  var
+    resultado: Float32;
+begin
+ resultado := somar(StrToFloatDef(edtValor1.Text, 0), StrToFloatDef(edtValor2.Text, 0));
+  lblResultado.Caption := resultado.ToString	;
+end;
+
+
+procedure TForm1.btnSubtrairClick(Sender: TObject);
+var
+  resultado: float32;
+begin
+     resultado:= subtrair(StrToFloatDef(edtValor1.Text, 0), StrToFloatDef(edtValor2.Text, 0));
+     lblResultado.Caption := resultado.ToString;
+end;
+
+
+
+//Funções
+
+function TForm1.somar(valor1: Float32; valor2: Float32): Float32;
+    begin
+      result :=  valor1 + valor2;
+    end;
+
+
+function TForm1.subtrair(valor1, valor2: Float32): Float32;
+  begin
+     result:= valor1 - valor2;
+  end;
+
+end.
+
+
